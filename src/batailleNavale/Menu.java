9644544c7d8f12ef5ae2c47e8b718db598f0407c
Nicolas Airault <nicolas.airault@etu.unilim.fr>
2018-04-26 20:00:40 +0200

    modification menubar
    ajout des actions sur la barre de menus

--- src/batailleNavale/Menu.java
+++ src/batailleNavale/Menu.java
@@ -1,35 +1,51 @@
 package batailleNavale;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 @SuppressWarnings("serial")
 public class Menu extends JMenuBar {
 	private JMenu jeu;
-	private JMenu aide;
-	
-	private JMenuItem start;
 	private JMenuItem quitter;
 	private JMenuItem regles;
-	
-	public Menu(){
-		
-		this.jeu = new JMenu("Jeu");
-		this.aide = new JMenu("?");
-		this.start = new JMenuItem("Commencer une partie");
+
+	public Menu() {
+		this.jeu = new JMenu("Jeu de Bataille Navale");
 		this.quitter = new JMenuItem("Quitter la partie");
+		this.quitter.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+
+			}
+		});
 		this.regles = new JMenuItem("Règles");
-		
-		
-		
-		this.jeu.add(start);
+		this.regles.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new JOptionPane();
+				JOptionPane.showMessageDialog(null,
+						"Vous disposez d'une grille 10x10. \n" + "Vous devez couler les navires de votre adversaire. \n"
+								+ "Pour ce faire, vous disposez vous aussi de navires que vous devez placer \n"
+								+ "sur votre grille. \n"
+								+ "Une fois les deux flottes posées, un joueur après l'autre devra décider d'une case \n"
+								+ "de la grille adverse sur laquelle il souhaite tirer.\n"
+								+ "Lorsque toute la flotte d'un des deux joueurs a coulé, c'est la fin de la partie.",
+						"Regles du jeu", JOptionPane.INFORMATION_MESSAGE);
+			}
+
+		});
+
+		this.jeu.add(regles);
 		this.jeu.addSeparator();
 		this.jeu.add(quitter);
-		this.aide.add(regles);
-		
 		this.add(jeu);
-		this.add(aide);
-		
 	}
 }
